aa74d88cb26f3e27a4870d2645e9f2683d1d37af
matt knox <matt.knox@reddit.com>
2024-02-27 11:11:01 -0800

    wired up comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.create! params.required(:comment).permit(:content)
+    redirect_to @post, notice: "Comment was successfully created."
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
